9f3f2fe95d6a2b6f42dc6efbc659b1f2ec9344b6
joseucarvajal <joseucarvajal@gmail.com>
2020-11-03 22:49:18 -0500

    Email services

---
+++ App.Common/Email/SendEmailSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Common.Email
+{
+    public class SendEmailSettings
+    {
+        public string Server { get; set; }
+        public string Port { get; set; }
+        public string SenderName { get; set; }
+        public string SenderEmail { get; set; }
+    }
+}
